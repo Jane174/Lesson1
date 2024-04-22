62833ec678c502c20cd171a9a4095c166696a2bf
Evgeniya <jannee174@gmail.com>
2024-04-22 21:50:53 +0500

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 2;
+int b = 7;
+int c = 8;
+int d = 3;
+
+int product = a * b;
+int sum = c + d;
+int quotient = product / sum;
+
+Console.WriteLine(product);
+Console.WriteLine(sum);
+Console.WriteLine(quotient);
